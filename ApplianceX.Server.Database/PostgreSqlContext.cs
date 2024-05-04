0104a1f21dcbeb7948ce9204fc121f3b2c5da49a
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-04 22:48:36 +0300

    init

---
+++ ApplianceX.Server.Database/PostgreSqlContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ApplianceX.Server.Database;
+
+public class PostgreSqlContext : DbContext
+{
+    public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options, ILoggerFactory loggerFactory) : base(options)
+    { 
+    }
+}
---
+++ ApplianceX.Server.Host/Startup.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using ApplianceX.Server.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+
+namespace ApplianceX.Server.Host;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    
+    private readonly ILoggerFactory _loggerFactory;
+    
+    
+    public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
+    {
+        Configuration = configuration;
+        _loggerFactory = loggerFactory;
+    }
+    
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        
+        var typeOfContent = typeof(Startup);
+        services.AddDbContext<PostgreSqlContext>(
+            opt => opt.UseNpgsql(
+                Configuration.GetConnectionString("PostgreSqlConnection"),
+                b => b.MigrationsAssembly(typeOfContent.Assembly.GetName().Name)
+            )
+        ); 
+        
+        
+
+        ConfigureSwagger(services);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApplianceX.Host v1"));
+        }
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+
+    public void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(
+            c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "ApplianceX.Host", Version = "v1"});
+            }
+        );
+    }
+}
