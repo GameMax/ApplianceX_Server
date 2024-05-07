550816a3136c486942e59b85c4fc57d1e379b0c9
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-07 16:59:26 +0300

    [+] Added 2 migrations

--- ApplianceX.Server.Database/PostgreSqlContext.cs
+++ ApplianceX.Server.Database/PostgreSqlContext.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApplianceX.Server.Database.Fridge;
+using ApplianceX.Server.Database.Seller;
+using ApplianceX.Server.Database.Seller.Statistic;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ApplianceX.Server.Database;
 
 public class PostgreSqlContext : DbContext
 {
+    public readonly DatabaseFacade Db;
+    
+    
+    public DbSet<SellerModel> Seller { get; set; }
+    public DbSet<SellerStatisticModel> SellerStatistic { get; set; }
+    // public DbSet<FridgeModel> Fridge { get; set; }
+    
+    
+
     public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options, ILoggerFactory loggerFactory) : base(options)
-    { 
+    {
+        Db = new DatabaseFacade(this, loggerFactory);
     }
 }
---
+++ ApplianceX.Server.Host/Migrations/20240507130335_sellerStatistic.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ApplianceX.Server.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class sellerStatistic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SellerStatistic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SellerId = table.Column<int>(type: "integer", nullable: false),
+                    Service = table.Column<int>(type: "integer", nullable: false),
+                    Delivery = table.Column<int>(type: "integer", nullable: false),
+                    Relevance = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SellerStatistic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SellerStatistic_Seller_SellerId",
+                        column: x => x.SellerId,
+                        principalTable: "Seller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SellerStatistic_SellerId",
+                table: "SellerStatistic",
+                column: "SellerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SellerStatistic");
+        }
+    }
+}
