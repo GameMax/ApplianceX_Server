e4205d5de62d75fb097fba694bbc5c943e948ed5
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-07 17:18:52 +0300

    [+] Abstract Client Controller

---
+++ ApplianceX.Server.Host/Controllers/Client/AbstractClientController.cs
@@ -0,0 +1,16 @@
+﻿using ApplianceX.Server.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApplianceX.Server.Host.Controllers.Client;
+
+
+[ApiController]
+[ApiExplorerSettings(GroupName = "v1")]
+[Route("api/v1/[controller]/[action]")]
+public abstract class AbstractClientController<T> : AbstractController<T> where T : ControllerBase
+{
+    protected AbstractClientController(PostgreSqlContext context, ILoggerFactory loggerFactory) : base(context, loggerFactory)
+    {
+    }
+}
