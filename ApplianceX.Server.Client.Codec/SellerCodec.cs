50fae4d175ef09a95fc561accb3b324aa897e983
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-07 17:20:01 +0300

    [+] Encode seller model

---
+++ ApplianceX.Server.Client.Codec/SellerCodec.cs
@@ -0,0 +1,30 @@
+﻿using ApplianceX.Server.Client.Payload.Seller;
+using ApplianceX.Server.Database.Seller;
+using ApplianceX.Server.Database.Seller.Statistic;
+
+namespace ApplianceX.Server.Client.Codec;
+
+public static class SellerCodec
+{
+    public static Payload.Seller.Seller EncodeSeller(SellerModel dbModel)
+    {
+        return new Payload.Seller.Seller
+        {
+            Id = dbModel.Id,
+            Name = dbModel.Name,
+            SellerStatisticPayload = EncodeSellerStatistic(dbModel.SellerStatistic)
+            
+        };
+    }
+
+
+    private static SellerStatistic EncodeSellerStatistic(SellerStatisticModel dbmodel)
+    {
+        return new SellerStatistic()
+        {
+            Delivery = dbmodel.Delivery,
+            Relevance = dbmodel.Relevance,
+            Service = dbmodel.Service
+        };
+    }
+}
