c0d18ad42a767cf6c82fae932e102dced741a8f1
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-07 17:20:56 +0300

    [+] Request: Create Seller

---
+++ ApplianceX.Server.Client/Seller/CreateSeller.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplianceX.Server.Client.Seller;
+
+public class CreateSeller
+{
+    [Required]
+    public string Name { get; set; }
+
+    public class Response
+    {
+        public Payload.Seller.Seller SellerPayload { get; set; }
+
+        public Response(Payload.Seller.Seller sellerPayload)
+        {
+            SellerPayload = sellerPayload;
+        }
+    }
+}
