8459bddb7d4d33e0559d4d5f605e465b4429cccf
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-07 17:19:38 +0300

    [+] Seller Payload [+] Seller Statistic Payload

---
+++ ApplianceX.Server.Client/Payload/Seller/Seller.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplianceX.Server.Client.Payload.Seller;
+
+public class Seller
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public SellerStatistic SellerStatisticPayload { get; set; }
+}
---
+++ ApplianceX.Server.Client/Payload/Seller/SellerStatistic.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplianceX.Server.Client.Payload.Seller;
+
+public class SellerStatistic
+{
+    public int Service { get; set; }
+
+    public int Delivery { get; set; }
+
+    public int Relevance { get; set; }
+}
