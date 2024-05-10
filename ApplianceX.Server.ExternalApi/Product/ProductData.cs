4b4a08a7617eb9eb60b148da3d2093aeacbc6122
Maksym Voznyi <mak7kas02@gmail.com>
2024-05-10 05:22:28 +0300

    [+] Product ids Dto

---
+++ ApplianceX.Server.ExternalApi/Product/ProductData.cs
@@ -0,0 +1,6 @@
+namespace ApplianceX.Server.ExternalApi.Product;
+
+public class ProductData
+{
+    public ProductInfo Data { get; set; }
+}
---
+++ ApplianceX.Server.ExternalApi/Product/ProductInfo.cs
@@ -0,0 +1,6 @@
+namespace ApplianceX.Server.ExternalApi.Product;
+
+public class ProductInfo
+{
+    public int[] Ids { get; set; }
+}
